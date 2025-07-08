58a132d83d222ffbb1894f87b9ba5f2ca4600dd5
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-08 20:09:18 +0100

    commit

--- e-commerce-frontend/src/components/CartItem.jsx
+++ e-commerce-frontend/src/components/CartItem.jsx
@@ -9,3 +9,4 @@ const CartItem = () => {
 }
 
 export default CartItem
+
--- e-commerce-frontend/src/components/Footer.jsx
+++ e-commerce-frontend/src/components/Footer.jsx
@@ -1,11 +1,90 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
-      FOOTER
-    </div>
-  )
-}
+    <footer>
+      <div>
+        <div>
+          <h3>Company info</h3>
+          <ul>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Shop Like a Billionaire</a></li>
+            <li><a href="#">Affiliate & Influencer: Earn Commission</a></li>
+            <li><a href="#">Contact us</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">Press</a></li>
+            <li><a href="#">Tree Planting Program</a></li>
+          </ul>
+        </div>
 
-export default Footer
+        <div>
+          <h3>Customer service</h3>
+          <ul>
+            <li><a href="#">Return and refund policy</a></li>
+            <li><a href="#">Intellectual property policy</a></li>
+            <li><a href="#">Shipping info</a></li>
+            <li><a href="#">Report suspicious activity</a></li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>Help</h3>
+          <ul>
+            <li><a href="#">Support center & FAQ</a></li>
+            <li><a href="#">Safety center</a></li>
+            <li><a href="#">Purchase protection</a></li>
+            <li><a href="#">Sitemap</a></li>
+            <li><a href="#">Partner with Us</a></li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>Download the App</h3>
+          <ul>
+            <li><a href="#">Price-drop alert</a></li>
+            <li><a href="#">Faster and more secure checkout</a></li>
+            <li><a href="#">Exclusive offer</a></li>
+            <li><a href="#">Sitemap</a></li>
+            <li><a href="#">Partner with Us</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <h4>Follow us</h4>
+          <div>
+            <ion-icon name="logo-instagram"></ion-icon>
+            <ion-icon name="logo-facebook"></ion-icon>
+            <ion-icon name="logo-twitter"></ion-icon>
+            <ion-icon name="logo-youtube"></ion-icon>
+            <ion-icon name="logo-tiktok"></ion-icon>
+            <ion-icon name="logo-pinterest"></ion-icon>
+            <ion-icon name="logo-linkedin"></ion-icon>
+            <ion-icon name="logo-whatsapp"></ion-icon>
+          </div>
+        </div>
+
+        <div>
+          <h4>We accept</h4>
+          <div>
+            <img src="images/219cc18d-0462-47ae-bf84-128d38206065.png.slim.webp" alt="payment method" />
+            <img src="images/3a626fff-bbf7-4a26-899a-92c42eef809a.png.slim.webp" alt="payment method" />
+            <img src="images/936bf9dc-9bb2-4935-9c5a-a70b800d4cf1.png.slim.webp" alt="payment method" />
+            <img src="images/a28c1d4b-4d3d-4df3-a83e-8bf72462368d.png.slim.webp" alt="payment method" />
+            <img src="images/b79a2dc3-b089-4cf8-a907-015a25ca12f2.png.slim.webp" alt="payment method" />
+            <img src="images/c3e5eb19-1b60-4c2b-87e1-4528fb390cbf.png.slim.webp" alt="payment method" />
+            <img src="images/da7f463a-916f-4d91-bcbb-047317a1c35e.png.slim.webp" alt="payment method" />
+            <img src="images/fb599a1d-6d42-49f2-ba7a-64b16d01b226.png.slim.webp" alt="payment method" />
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <p>&copy; 2023 E-commerce Group Project. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

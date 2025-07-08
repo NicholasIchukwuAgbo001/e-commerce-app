e03a7d1fd06cc45e575de88a9edfda0ebd749ab6
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-08 21:12:38 +0100

    Enhances layout structure of the Home component
    Improves the Home component's layout by adding a flexbox structure for better responsiveness.
    
    Introduces a main section that utilizes the Outlet for nested routes, allowing for dynamic content rendering within the layout.
    
    This change ensures a more organized and visually appealing user interface.

--- e-commerce-frontend/src/pages/Home.jsx
+++ e-commerce-frontend/src/pages/Home.jsx
@@ -1,13 +1,21 @@
+import { Outlet } from "react-router-dom"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 
 const Home = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
+
+      <main className="flex-grow overflow-y-auto">
+        <div className="mx-auto max-w-6xl px-4 py-6">
+          <Outlet />
+        </div>
+      </main>
+
       <Footer />
-    </div>
-  )
+    </div> 
+  ) 
 }
 
 export default Home

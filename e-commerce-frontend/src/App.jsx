fe0c70de4134a70cdf8615f86c4d2d006655106b
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-08 14:04:54 +0100

    commit

--- e-commerce-frontend/src/App.jsx
+++ e-commerce-frontend/src/App.jsx
@@ -1,9 +1,27 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
-    <div>
-      Hello
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Header />
+      <main className="min-h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;

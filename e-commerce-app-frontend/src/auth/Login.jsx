30f25cddad8bd860c86c85ed179d4fa8fdcc944e
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-19 11:26:51 +0100

    commit

--- e-commerce-app-frontend/src/auth/Login.jsx
+++ e-commerce-app-frontend/src/auth/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ProductList = () => {
+const Login = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const ProductList = () => {
   )
 }
 
-export default ProductList
+export default Login

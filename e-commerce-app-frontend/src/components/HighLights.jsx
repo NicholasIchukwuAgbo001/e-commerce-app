1b394e8394479dd560507abde44301a3fe3f6cde
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-19 15:00:48 +0100

    commit

--- e-commerce-app-frontend/src/components/HighLights.jsx
+++ e-commerce-app-frontend/src/components/HighLights.jsx
@@ -1,19 +1,18 @@
-const highlights = [
-  { label: "Shop"},
-  { label: "Sale"},
-  { label: "Brands"}
-];
+import { IoChevronDownOutline } from "react-icons/io5";
 
 const Highlights = () => {
   return (
     <div className="flex gap-4 items-center justify-center text-sm">
-      {highlights.map(({ label }) => (
-        <div
-          key={label}
-          className="flex items-center gap-1 hover:bg-stone-100 p-3 rounded-full cursor-pointer transition"
-        >
-        </div>
-      ))}
+      <div className="flex items-center gap-1 hover:bg-stone-100 p-3 rounded-full cursor-pointer">
+        <span>Shop</span>
+        <IoChevronDownOutline className="text-stone-900 text-xl" />
+      </div>
+      <div className="flex items-center gap-1 hover:bg-stone-100 p-3 rounded-full cursor-pointer">
+        <span>Sale...</span>
+      </div>
+      <div className="hover:bg-stone-100 p-3 rounded-full cursor-pointer">
+        <span>Brands</span>
+      </div>
     </div>
   );
 };

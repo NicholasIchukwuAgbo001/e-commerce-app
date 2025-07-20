f1c645377e7449eae467c216ff80146213be27da
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-20 14:55:01 +0100

    commit

--- e-commerce-frontend/src/components/TopBar.jsx
+++
@@ -1,9 +0,0 @@
-const TopBar = () => {
-  return (
-    <div className="flex flex-col sm:flex-row justify-between items-center gap-4 px-4 py-5 bg-stone-950 text-sm px-">
-
-    </div>
-  );
-};
-
-export default TopBar;

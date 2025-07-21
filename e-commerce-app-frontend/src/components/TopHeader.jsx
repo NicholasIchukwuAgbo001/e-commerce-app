e4fb5550afb0f39f47fd26decf1501756aea4d75
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-21 07:12:40 +0100

    commit

--- e-commerce-app-frontend/src/components/TopHeader.jsx
+++ e-commerce-app-frontend/src/components/TopHeader.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router"
 const TopHeader = () => {
   return (
     <div className="bg-stone-900 h-8 flex items-center justify-center ">
-      <p className="text-white font-sm">Sign up and get 20% off to your first order. <Link className="underline">Sing Up Now</Link> </p>
+      <p className="text-white font-sm">Sign up and get 20% off to your first order. <Link className="underline" to="/signup">Sing Up Now</Link> </p>
     </div>
   )
 }

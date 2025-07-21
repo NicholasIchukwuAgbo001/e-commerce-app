a83e42ab249bf0f5e76618dfaf1718769b39985a
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-21 22:31:43 +0100

    Adds search functionality to product listings
    Integrates a search bar component to filter displayed products based on user input.
    
    Enhances user experience by allowing real-time search capabilities, displaying relevant products dynamically.
    
    Also includes error handling for product fetching.
    
    Adds real-time search functionality to product listings
    
    Integrates a search bar component to allow users to filter products dynamically based on their input.
    
    Enhances user experience with real-time search capabilities and includes error handling for product fetching.
    
    No products matching the search criteria will display an appropriate message.

--- e-commerce-app-frontend/src/components/SearchBar.jsx
+++ e-commerce-app-frontend/src/components/SearchBar.jsx
@@ -1,15 +1,20 @@
-import { HiOutlineSearch } from "react-icons/hi" 
+import { HiOutlineSearch } from "react-icons/hi";
 
-const SearchBar = () => {
+const SearchBar = ({ SearchItems, setSearchItem }) => {
+  const handleSearch = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form className="relative w-full">
-      <HiOutlineSearch className="absolute top-1/3 left-4" />
+    <form className="relative w-full" onSubmit={handleSearch}>
+      <HiOutlineSearch className="absolute top-1/3 left-4 text-gray-500" />
       <input
         type="text"
         placeholder="Search for products..."
         className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-stone-500 shadow-sm text-sm sm:text-base"
+        value={SearchItems}
+        onChange={(e) => setSearchItem(e.target.value)}
       />
-      
     </form>
   );
 };

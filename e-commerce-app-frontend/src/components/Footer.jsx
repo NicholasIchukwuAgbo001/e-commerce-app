f7a11b7038dd0ea6e92d3c7525bcc010dac12400
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-23 13:02:34 +0100

    commit

--- e-commerce-app-frontend/src/components/Footer.jsx
+++ e-commerce-app-frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import FooterSocials from "./FooterSocials";
 const Footer = () => {
   return (
     <footer className="bg-stone-300 text-black text-sm px-28 pt-5">
-      <div className="flex flex-wrap justify-between gap-8 px-6 py-10">
+      <div className="fle flex-wrap justify-between gap-8 px-6 py-10">
         <FooterLinksSection />
       </div>
 

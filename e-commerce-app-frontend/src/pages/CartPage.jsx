f493ce85d0ceb69af3c21fb176d07d7832199f99
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-23 09:27:51 +0100

    commit

--- e-commerce-app-frontend/src/pages/CartPage.jsx
+++ e-commerce-app-frontend/src/pages/CartPage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { removeFromCart, incrementQuantity, decrementQuantity } from "../api/cartSlice";
 import { MdDelete } from "react-icons/md"
+import CartSummary from "../components/CartSummary";
 
 const CartPage = () => {
   const dispatch = useDispatch();
@@ -10,12 +11,6 @@ const CartPage = () => {
   useEffect(() => {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }, [cartItems]);
-
-  const totalAmount = cartItems.reduce(
-    (acc, item) => acc + item.price * item.quantity,
-    0
-  );
-  const totalQty = cartItems.reduce((acc, item) => acc + item.quantity, 0);
 
   return (
     <div className="px-4 py-10 max-w-7xl mx-auto">
@@ -67,32 +62,7 @@ const CartPage = () => {
               </div>
             ))}
           </div>
-
-          <div className="bg-stone-50 rounded-xl p-6 shadow-md h-fit">
-            <h3 className="font-semibold text-lg mb-4">Order Summary</h3>
-            <div className="flex justify-between text-gray-600">
-              <span>Total Items</span>
-              <span>{totalQty}</span>
-            </div>
-            <div className="flex justify-between mt-2 font-medium">
-              <span>Total</span>
-              <span>${totalAmount.toFixed(2)}</span>
-            </div>
-            <form className="py-2">
-              <input 
-              type="text" 
-              name="promo" 
-              id="promo" 
-              placeholder=" Add promo code" 
-              className="py-2 px-4 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-stone-500 shadow-sm text-sm sm:text-base"
-              required
-              />
-              <button className="bg-black text-white py-2 px-4 ml-8 rounded-full">Apply</button>
-            </form>
-            <button className="w-full bg-black text-white py-2 rounded-full hover:opacity-90 transition">
-             Go to Checkout
-            </button>
-          </div>
+          <CartSummary />
         </div>
       )}
     </div>

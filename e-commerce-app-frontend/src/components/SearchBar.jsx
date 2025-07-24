6e9c80a41a1e2bf317cdfdf6cf63847faef4119c
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-07-24 20:21:36 +0100

    commit

--- e-commerce-app-frontend/src/components/SearchBar.jsx
+++ e-commerce-app-frontend/src/components/SearchBar.jsx
@@ -10,7 +10,7 @@ const SearchBar = ({ searchItem, setSearchItem }) => {
       <HiOutlineSearch className="absolute top-1/3 left-4 text-gray-500" />
       <input
         type="text"
-        placeholder="Search for products..."
+        placeholder="Search for products."
         className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-green-800 shadow-sm text-sm sm:text-base"
         value={searchItem}
         onChange={(event) => setSearchItem(event.target.value)}
